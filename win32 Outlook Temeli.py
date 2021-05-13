06451077bf0337328ca0fd49067559c0f8850234
yacaat <37926237+yacaat@users.noreply.github.com>
2021-05-13 21:43:49 +0300

    Add files via upload

---
+++ win32 Outlook Temeli.py
@@ -0,0 +1,11 @@
+import win32com.client
+
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+
+inbox = outlook.GetDefaultFolder(6) # "6" refers to the index of a folder - in this case,
+                                    # the inbox. You can change that number to reference
+                                    # any other folder
+messages = inbox.Items
+message = messages.GetLast()
+body_content = message.body
+print(body_content)
